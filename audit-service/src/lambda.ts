e780fc792a30448528805b81d325863c90fa659a
shuhaib-s <dev.shuhaib@gmail.com>
2026-05-24 11:12:35 +0530

    Implement SQS trigger in audit-service to process messages and store them in DynamoDB, while maintaining API Gateway functionality.

--- audit-service/src/lambda.ts
+++ audit-service/src/lambda.ts
@@ -1,4 +1,67 @@
 import serverless from "serverless-http";
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app";
 
-export const handler = serverless(app);
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+import {
+    DynamoDBDocumentClient,
+    PutCommand
+} from "@aws-sdk/lib-dynamodb";
+
+const expressHandler = serverless(app);
+
+const client = new DynamoDBClient({
+    region: process.env.REGION
+});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const TABLE_NAME = process.env.DYNAMO_TABLE_NAME!;
+
+export const handler = async (event:any, context:any) => {
+
+    try {
+
+        // SQS Trigger
+        if (
+            event.Records &&
+            event.Records[0]?.eventSource === "aws:sqs"
+        ) {
+
+            for (const record of event.Records) {
+
+                const body = JSON.parse(record.body);
+
+                console.log("SQS EVENT:", body);
+
+                await dynamo.send(
+                    new PutCommand({
+                        TableName: TABLE_NAME,
+                        Item: {
+                            id: crypto.randomUUID(),
+                            ...body
+                        }
+                    })
+                );
+            }
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "sqs processed"
+                })
+            };
+        }
+
+        // API Gateway Trigger
+        return expressHandler(event, context);
+
+    } catch (err) {
+
+        console.error(err);
+
+        throw err;
+    }
+};
